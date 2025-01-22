df05e486d80eb6aecd2d22f5dc99c61bf96c14c0
poria neyshaboriyan <poria.nsh@gmail.com>
2025-01-22 09:15:52 +0330

    update to work

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,8 +3,7 @@ import {XCircleIcon} from "@heroicons/react/20/solid";
 function Modal({ isopen, setisopen, title, children }) {
     if (!isopen) return null;
   return (
-    <div>
-      <div className="backdrop" onClick={() => setisopen(false)}></div>
+    <div className="backdrop" onClick={() => setisopen(false)}>
       <div className="modal">
         <div className="modal__header">
           <h2 className="title">{title}</h2>
