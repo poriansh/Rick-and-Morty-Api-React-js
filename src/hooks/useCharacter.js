0bc683f9a99e733c245a571336a9a6b9c50fa8bd
poria neyshaboriyan <poria.nsh@gmail.com>
2025-03-16 21:32:33 +0330

    test

--- src/hooks/useCharacter.js
+++ src/hooks/useCharacter.js
@@ -1,28 +1,29 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
 function useCharacter(query) {
-  // const contoroler = new AbortController();
-  // const signal = contoroler.signal;
+  const contoroler = new AbortController();
+  const signal = contoroler.signal;
   const [Characters, setCharacter] = useState([]);
   useEffect(() => {
     async function fetchData() {
       try {
-        const {data} = await axios.get(`https://rickandmortyapi.com/api/character/?name=${query}`);
+        const {data} = await axios.get(`https://rickandmortyapi.com/api/character/?name=${query}`,{signal});
        if (data.results?.length > 0) {
          setCharacter(data.results.slice(0, 5));
        } else {
          setCharacter([]); // در صورت نبودن کاراکتر، آرایه خالی برگردون
        }
       } catch (err) {
-        // if (axios.isCancel()) {
-        // }
-        setCharacter([]);
+        if (!axios.isCancel()) {
+
+          setCharacter([]);
+        }
       }
     }
     fetchData();
-    // return () => {
-    //   contoroler.abort();
-    // };
+    return () => {
+      contoroler.abort();
+    };
   }, [query]);
   return {Characters}
 }
